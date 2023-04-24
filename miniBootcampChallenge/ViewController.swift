f11fef7d96ad2e84bbdec3893f417c91392b7f40
Liimmaa <halimammegwa@gmail.com>
2023-04-24 22:50:14 +0100

    Implement a function to download the images in a smooth way that allows a good UX when scrolling the UICollectionView

--- miniBootcampChallenge/ViewController.swift
+++ miniBootcampChallenge/ViewController.swift
@@ -21,28 +21,25 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         title = Constants.title
     }
-
-
-}
-
-
-// TODO: 1.- Implement a function that allows the app downloading the images without freezing the UI or causing it to work unexpected way
-// For non-file URLS, using the dataTask(with:completionHandler:) method of the URLSession is a better option. Then, loading the image in DispatchQueue.main.async loads the images asynchronously.
-
-func getImage(url: URL, completion: @escaping (UIImage?) -> Void) {
-    URLSession.shared.dataTask(with: url) { data, response, error in
-        if let data = data, let image = UIImage(data: data) {
-            DispatchQueue.main.async {
-                completion(image)
+    
+    
+    // TODO: 1.- Implement a function that allows the app downloading the images without freezing the UI or causing it to work unexpected way
+    // For non-file URLS, using the dataTask(with:completionHandler:) method of the URLSession is a better option. Then, loading the image in DispatchQueue.main.async loads the images asynchronously.
+    
+    func getImage(url: URL, completion: @escaping (UIImage?) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            } else {
+                completion(nil)
             }
-        } else {
-            completion(nil)
-        }
-    }.resume()
+        }.resume()
+    }
+    
+    // TODO: 2.- Implement a function that allows to fill the collection view only when all photos have been downloaded, adding an animation for waiting the completion of the task.
 }
-
-// TODO: 2.- Implement a function that allows to fill the collection view only when all photos have been downloaded, adding an animation for waiting the completion of the task.
-
 
 // MARK: - UICollectionView DataSource, Delegate
 extension ViewController {
@@ -53,10 +50,12 @@ extension ViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellID, for: indexPath) as? ImageCell else { return UICollectionViewCell() }
         
+        // Implementation for the first function
         let url = urls[indexPath.row]
         getImage(url: url) { image in
             cell.display(image)
         }
+        
         return cell
     }
 }
@@ -66,17 +65,17 @@ extension ViewController {
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if Constants.cellSize == nil {
-          let layout = collectionViewLayout as! UICollectionViewFlowLayout
+            let layout = collectionViewLayout as! UICollectionViewFlowLayout
             let emptySpace = layout.sectionInset.left + layout.sectionInset.right + (Constants.columns * Constants.cellSpacing - 1)
             Constants.cellSize = (view.frame.size.width - emptySpace) / Constants.columns
         }
         return CGSize(width: Constants.cellSize!, height: Constants.cellSize!)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         Constants.cellSpacing
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         Constants.cellSpacing
     }
