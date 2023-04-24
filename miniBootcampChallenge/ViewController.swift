916731a242810b48a40ec506c601553b47bde793
Liimmaa <halimammegwa@gmail.com>
2023-04-24 22:57:28 +0100

    Implement method to wait until all images are downloaded before to display all in the UICollectionView

--- miniBootcampChallenge/ViewController.swift
+++ miniBootcampChallenge/ViewController.swift
@@ -16,10 +16,27 @@ class ViewController: UICollectionViewController {
     }
     
     private lazy var urls: [URL] = URLProvider.urls
+    let activityIndicator =  UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Constants.title
+        setupSubview()
+        
+        //For the second function
+        downloadImages(urls: urls) {
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func setupSubview() {
+        self.view.addSubview(activityIndicator)
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.center = self.view.center
+        activityIndicator.startAnimating()
     }
     
     
@@ -39,6 +56,42 @@ class ViewController: UICollectionViewController {
     }
     
     // TODO: 2.- Implement a function that allows to fill the collection view only when all photos have been downloaded, adding an animation for waiting the completion of the task.
+    // Use the dataTask(with:completionHandler:) method of the URLSession and creating a dispatch queue to keep track of when all downloads are complete
+    //  and save to FileManager. After download has been completed, it send the data to the completion handler.
+    func downloadImages(urls: [URL], completion: @escaping () -> Void) {
+        let fileManager = FileManager.default
+        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        // Create a DispatchGroup to keep track of when all downloads are complete
+        let group = DispatchGroup()
+        
+        for (index, url) in urls.enumerated() {
+            group.enter()
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                defer { group.leave() }
+                
+                guard let data = data else {
+                    return
+                }
+                
+                let fileName = "\(index).jpg"
+                let fileURL = documentsDirectory.appendingPathComponent(fileName)
+                
+                do {
+                    try data.write(to: fileURL)
+                    print("Downloaded and saved image to \(fileURL)")
+                } catch {
+                    print("Error saving image to file: \(error)")
+                }
+            }.resume()
+        }
+        
+        // Notify the completion handler when all downloads are complete
+        group.notify(queue: DispatchQueue.main) {
+            completion()
+        }
+    }
 }
 
 // MARK: - UICollectionView DataSource, Delegate
@@ -51,8 +104,17 @@ extension ViewController {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.cellID, for: indexPath) as? ImageCell else { return UICollectionViewCell() }
         
         // Implementation for the first function
-        let url = urls[indexPath.row]
-        getImage(url: url) { image in
+//        let url = urls[indexPath.row]
+//        getImage(url: url) { image in
+//            self.activityIndicator.stopAnimating()
+//            cell.display(image)
+//        }
+        
+        // Implementation for the second function
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileName = "\(indexPath.row).jpg"
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        if let image = UIImage(contentsOfFile: fileURL.path) {
             cell.display(image)
         }
         
